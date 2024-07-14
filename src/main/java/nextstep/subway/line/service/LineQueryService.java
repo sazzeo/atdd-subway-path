47dffc370cfc4af369d696b33c344e3f748a6c38
jiyeong-dt <imjee02@gmail.com>
2024-07-14 23:44:28 +0900

    feat(step1): 역 정렬해서 반환하도록 매핑 코드 추가후 기존 코드 리팩토링

--- src/main/java/nextstep/subway/line/service/LineQueryService.java
+++ src/main/java/nextstep/subway/line/service/LineQueryService.java
@@ -1,14 +1,15 @@
 package nextstep.subway.line.service;
 
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.exception.LineNotFoundException;
 import nextstep.subway.line.payload.LineResponse;
 import nextstep.subway.line.repository.LineRepository;
 import nextstep.subway.station.domain.Station;
 import nextstep.subway.station.repository.StationRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -21,7 +22,6 @@ public class LineQueryService {
     private final LineRepository lineRepository;
     private final StationRepository stationRepository;
 
-
     public LineQueryService(final LineRepository lineRepository, final StationRepository stationRepository) {
         this.lineRepository = lineRepository;
         this.stationRepository = stationRepository;
@@ -33,24 +33,28 @@ public class LineQueryService {
                 .flatMap(line -> line.getStationIds().stream())
                 .collect(Collectors.toSet());
 
-        Map<Long, Station> stationMap = stationRepository.findByIdIn(stationsIds)
-                .stream()
-                .collect(Collectors.toMap(Station::getId, (station -> station)));
-
         return lines.stream()
                 .map(line ->
-                        LineResponse.from(line,
-                                stationsIds.stream()
-                                        .map(stationMap::get)
-                                        .collect(Collectors.toList()))
+                        LineResponse.from(line, getSortedStations(stationsIds))
                 ).collect(Collectors.toList());
-
     }
 
     public LineResponse getLine(final Long id) {
         Line line = getById(id);
-        return LineResponse.from(line, getLineStations(line));
+        return LineResponse.from(line, getSortedStations(line.getStationIds()));
+    }
 
+    private Map<Long, Station> getStationMap(final Collection<Long> stationsIds) {
+        return stationRepository.findByIdIn(stationsIds)
+                .stream()
+                .collect(Collectors.toMap(Station::getId, (station -> station)));
+    }
+
+    private List<Station> getSortedStations(final Collection<Long> stationsIds) {
+        Map<Long, Station> stationMap = getStationMap(stationsIds);
+        return stationsIds.stream()
+                .map(stationMap::get)
+                .collect(Collectors.toList());
     }
 
     private Line getById(final Long id) {
@@ -58,7 +62,4 @@ public class LineQueryService {
                 .orElseThrow(() -> new LineNotFoundException("존재하지 않는 지하철 노선입니다."));
     }
 
-    private List<Station> getLineStations(final Line line) {
-        return stationRepository.findByIdIn(line.getStationIds());
-    }
 }
