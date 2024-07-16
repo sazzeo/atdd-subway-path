5070dc1a0926f0dc6384d5710f779a0293f78066
jiyeong-dt <imjee02@gmail.com>
2024-07-16 23:44:48 +0900

    test(step1): HttpStatusAssertion 유틸 클래스 추가

---
+++ src/test/java/nextstep/subway/utils/HttpStatusAssertion.java
@@ -0,0 +1,20 @@
+package nextstep.subway.utils;
+
+import org.springframework.http.HttpStatus;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HttpStatusAssertion {
+
+    public static void assertOk(int status) {
+        assertThat(status).isEqualTo(HttpStatus.OK.value());
+    }
+
+    public static void assertNoContent(int status) {
+        assertThat(status).isEqualTo(HttpStatus.NO_CONTENT.value());
+    }
+
+    public static void assertBadRequest(int status) {
+        assertThat(status).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+}
