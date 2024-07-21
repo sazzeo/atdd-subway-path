83a9302df1cb8411ab01d8249a75d562b65ab07c
jiyeong-dt <imjee02@gmail.com>
2024-07-21 23:19:54 +0900

    refactor(step2): sorter static으로 클래스로 변경

---
+++ src/test/java/nextstep/subway/line/domain/StationSorterTest.java
@@ -0,0 +1,36 @@
+package nextstep.subway.line.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+class StationSorterTest {
+
+    private List<Section> sections;
+
+    @BeforeEach
+    void setUp() {
+        //Given 정렬되지 않은 구간의 역을
+        sections = new ArrayList<>();
+        sections.add(new Section(1L, 2L, 10L));
+        sections.add(new Section(3L, 4L, 10L));
+        sections.add(new Section(2L, 3L, 10L));
+    }
+
+    @Test
+    @DisplayName("정렬되지 않은 구간의 역을 순서대로 반환한다")
+    void test() {
+        //when 정렬 메소드 실행시
+        var sortedStationIds = StationSorter.getSortedStationIds(sections);
+        //then 순서대로 반환한다
+        assertThat(sortedStationIds).containsExactly(1L, 2L, 3L, 4L);
+    }
+
+
+}
